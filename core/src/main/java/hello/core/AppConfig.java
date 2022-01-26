e3f58bcbc51b508cb0cedac2da6d5a2c89b4f493
LeeSangBum <dltkdqja101@gmail.com>
2022-01-26 15:53:10 +0900

    22.01.25 구현객체를 직접 주입했던 코드에서 AppConfig 라는 생성자주입을 담당해주는 클래스를 만들어 사용자가 구체적인 클래스를 모르고도 사용할 수 있도록 코드를 변경 하였다. 생성자 주입과 역할의 분리에 대해 배움 22.01.26 스프링의 컨테이너 라고 불리는 설정 클래스 AppConfig 를 순수 자바 코드에서 @Configuration, @Bean 어노테이션과 ApplicationContext 타입의 AnnotationConfigApplicationContext(AppConfig.class) 를 활용하여 스프링컨테이너에 원하는 Bean 을 DI 시키도록 변경하였다.

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,12 +8,16 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     /**
      * @return memberService 의 구현체 생성 파라미터는 현재 사용중인 저장소
      */
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
@@ -21,13 +25,15 @@ public class AppConfig {
     /**
      * @return 저장소 구현체
      */
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     /**
      * @return OrderService 의 구현체 생성 파라미터는 현재 사용중인 저장소와 할인정책
      */
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
@@ -35,7 +41,8 @@ public class AppConfig {
     /**
      * @return 할인정책 구현체
      */
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
