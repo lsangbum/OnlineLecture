880c4f1fcacb7227df369f8f4811be5f713bb643
LeeSangBum <dltkdqja101@gmail.com>
2022-03-01 19:40:39 +0900

    22.03.01 -

--- jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @Slf4j  //Logger log = LoggerFactory.getLogger(getClass()); 선언문을 어노테이션으로 대체
 public class HomeController {
